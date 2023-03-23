b4875f55806d39725fa4659d331ff1789152477f
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-03-23 21:16:01 +0700

    railway: trigger deployment

--- config/seeder.go
+++ config/seeder.go
@@ -1,15 +1,17 @@
 package config
 
 import (
+	"errors"
+
 	"github.com/Caknoooo/golang-clean_template/entities"
 	"gorm.io/gorm"
 )
 
-func Seeder(db *gorm.DB) error{
+func Seeder(db *gorm.DB) error {
 	if err := ListBankSeeder(db); err != nil {
 		return err
 	}
-	
+
 	if err := StatusPembayaranSeeder(db); err != nil {
 		return err
 	}
@@ -17,37 +19,45 @@ func Seeder(db *gorm.DB) error{
 	return nil
 }
 
-func ListBankSeeder(db *gorm.DB) error{
+func ListBankSeeder(db *gorm.DB) error {
 	var listBanks = []entities.ListBank{
 		{
-			ID: 1, 
+			ID:   1,
 			Nama: "BNI",
 		},
 		{
-			ID: 2, 
-			Nama: "BCA", 
+			ID:   2,
+			Nama: "BCA",
 		},
 		{
-			ID: 3, 
+			ID:   3,
 			Nama: "Mandiri",
 		},
 		{
-			ID: 4,
+			ID:   4,
 			Nama: "BSI",
 		},
 		{
-			ID: 5, 
+			ID:   5,
 			Nama: "OVO",
-		}, 
+		},
 		{
-			ID: 6,
+			ID:   6,
 			Nama: "Gopay",
 		},
 	}
 
 	for _, data := range listBanks {
-		if err := db.Create(&data).Error; err != nil {
+		var bank entities.ListBank
+		err := db.Where(&entities.ListBank{ID: data.ID}).First(&bank).Error
+		if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
 			return err
+		}
+
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			if err := db.Create(&data).Error; err != nil {
+				return err
+			}
 		}
 	}
 
@@ -57,22 +67,30 @@ func ListBankSeeder(db *gorm.DB) error{
 func StatusPembayaranSeeder(db *gorm.DB) error {
 	var statusPembayaran = []entities.StatusPembayaran{
 		{
-			ID: 1, 
-			Status: "Failed", 
+			ID:     1,
+			Status: "Failed",
 		},
 		{
-			ID: 2, 
+			ID:     2,
 			Status: "Success",
-		}, 
+		},
 		{
-			ID: 3,
+			ID:     3,
 			Status: "Awaiting",
 		},
 	}
 
-	for _, data := range statusPembayaran{
-		if err := db.Create(&data).Error; err != nil {
+	for _, data := range statusPembayaran {
+		var status_pembayaran entities.StatusPembayaran
+		err := db.Where(entities.StatusPembayaran{ID: data.ID}).First(&status_pembayaran).Error
+		if err != nil && errors.Is(err, gorm.ErrRecordNotFound) {
 			return err
+		}
+
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			if err := db.Create(&data).Error; err != nil {
+				return err
+			}
 		}
 	}
 
