ec29bc20a850371d8417aac4af9a829ca9a41bf0
M Naufal Badruttamam <92671053+Caknoooo@users.noreply.github.com>
2023-03-27 22:31:38 +0700

    feat: add response image for all event response

--- dto/event.go
+++ dto/event.go
@@ -13,6 +13,7 @@ type EventCreateDTO struct {
 	DeskripsiEvent string    `json:"deskripsi_event" form:"deskripsi_event" binding:"required"`
 	JenisEvent     string    `json:"jenis_event" form:"jenis_event" binding:"required"`
 	JumlahDonasi   float64   `json:"jumlah_donasi" form:"jumlah_donasi" binding:"required"`
+	MaxDonasi      float64   `json:"max_donasi" form:"max_donasi" binding:"required"`
 	FotoEvent      string    `json:"foto_event" form:"foto_event" binding:"required"`
 	ExpiredDonasi  time.Time `json:"expired_donasi" form:"expired_donasi" binding:"required"`
 	UserID         uuid.UUID `json:"user_id" form:"user_id" binding:"required"`
@@ -29,4 +30,40 @@ type EventUpdateDTO struct {
 	UserID         *string   `json:"user_id" form:"user_id"`
 	IsTargetFull   *bool     `json:"is_target_full" form:"is_target_full"`
 	IsExpired      *bool     `json:"is_expired" form:"is_expired"`
+}
+
+type EventResponseServiceDTO struct {
+	ID             uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
+	Nama           string    `json:"nama" form:"nama"`
+	DeskripsiEvent string    `json:"deskripsi_event" form:"deskripsi_event"`
+	FotoEvent      string    `json:"foto_event" form:"foto_event"`
+	ExpiredDonasi  time.Time `json:"expired_donasi" form:"expired_donasi" binding:"required"`
+	IsExpired      bool      `json:"is_expired" form:"is_expired"`
+}
+
+type EventResponseDetailDonasiDTO struct {
+	Nama           string    `json:"nama" form:"nama"`
+	DeskripsiEvent string    `json:"deskripsi_event" form:"deskripsi_event"`
+	FotoEvent      string    `json:"foto_event" form:"foto_event"`
+	JumlahDonasi   float64   `json:"jumlah_donasi" form:"jumlah_donasi"`
+	MaxDonasi      float64   `json:"max_donasi" form:"max_donasi"`
+	ExpiredDonasi  time.Time `json:"expired_donasi" form:"expired_donasi" binding:"required"`
+	IsExpired      bool      `json:"is_expired" form:"is_expired"`
+	IsDone         uint64    `json:"is_done"`
+
+	// Mengeluarkan 3 orang yang terakhir donasi
+}
+
+type EventResponseListDonasiDTO struct {
+	Nama           string  `json:"nama" form:"nama"`
+	DeskripsiEvent string  `json:"deskripsi_event" form:"deskripsi_event"`
+	FotoEvent      string  `json:"foto_event" form:"foto_event"`
+	JumlahDonasi   float64 `json:"jumlah_donasi" form:"jumlah_donasi"`
+	MaxDonasi      float64 `json:"max_donasi" form:"max_donasi"`
+}
+
+type EventResponseMyEventDTO struct {
+	Nama           string  `json:"nama" form:"nama"`
+	DeskripsiEvent string  `json:"deskripsi_event" form:"deskripsi_event"`
+	FotoEvent      string  `json:"foto_event" form:"foto_event"`
 }
--- entities/event.go
+++ entities/event.go
@@ -7,17 +7,19 @@ import (
 )
 
 type Event struct {
-	ID               uuid.UUID          `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
-	RekeningEvent    string             `gorm:"type:varchar(100)" json:"rekening_event"`
-	Nama             string             `gorm:"type:varchar(100)" json:"nama"`
-	DeskripsiEvent   string             `gorm:"type:text" json:"deskripsi_event"`
-	JenisEvent       string             `gorm:"type:varchar(100)" json:"jenis_event"`
-	JumlahDonasi     float64            `gorm:"type:float" json:"jumlah_donasi"`
-	LikeCount        uint64             `json:"like_count"`
-	FotoEvent        string             `gorm:"type:varchar(100)" json:"foto_event"`
-	ExpiredDonasi    time.Time          `gorm:"datetime" json:"expired_donasi"`
-	Is_target_full   bool               `gorm:"type:boolean" json:"is_target_full"`
-	Is_expired       bool               `gorm:"type:boolean" json:"is_expired"`
+	ID             uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
+	RekeningEvent  string    `gorm:"type:varchar(100)" json:"rekening_event"`
+	Nama           string    `gorm:"type:varchar(100)" json:"nama"`
+	DeskripsiEvent string    `gorm:"type:text" json:"deskripsi_event"`
+	JenisEvent     string    `gorm:"type:varchar(100)" json:"jenis_event"`
+	JumlahDonasi   float64   `gorm:"type:float" json:"jumlah_donasi"`
+	MaxDonasi      float64   `gorm:"type:float" json:"max_donasi"`
+	LikeCount      uint64    `json:"like_count"`
+	FotoEvent      string    `gorm:"type:varchar(100)" json:"foto_event"`
+	ExpiredDonasi  time.Time `gorm:"datetime" json:"expired_donasi"`
+	IsDone         uint64    `json:"is_done"`
+	Is_target_full bool      `gorm:"type:boolean" json:"is_target_full"`
+	Is_expired     bool      `gorm:"type:boolean" json:"is_expired"`
 
 	UserID           uuid.UUID          `gorm:"type:uuid" json:"user_id"`
 	User             User               `gorm:"foreignKey:UserID" json:"user"`
