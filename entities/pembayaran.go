0d089430118f85c5505b3c2dd0127d73ca62e300
aptarr <apta.rasendriya@gmail.com>
2023-03-30 14:04:07 +0700

    feat: add services for transaksi and pembayaran

--- entities/pembayaran.go
+++ entities/pembayaran.go
@@ -1,21 +1,15 @@
 package entities
 
 import (
-	"time"
-
 	"github.com/google/uuid"
 )
 
-type Transaksi struct {
-	ID                  uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
-	Jumlah_Donasi_Event float64   `gorm:"type:float" json:"jumlah_donasi"`
-	Tanggal_Transaksi   time.Time `gorm:"datetime" json:"tangal_transaksi"`
+type Pembayaran struct {
+	ID              uuid.UUID `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
+	Jumlah          float64   `gorm:"type:float" json:"jumlah"`
+	BuktiPembayaran string    `gorm:"type:varchar(50)" json:"bukti_pembayaran"`
 
-	// HistoryTransaksiUser HistoryTransaksiUser `gorm:"foreignKey:TransaksiID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"history_transaksi_users,omitempty"`
-	EventID      uuid.UUID  `gorm:"type:uuid" json:"event_id"`
-	Event        Event      `gorm:"foreignKey:EventID" json:"event"`
-	PembayaranID uuid.UUID  `gorm:"type:uuid" json:"pembayaran_id"`
-	Pembayaran   Pembayaran `gorm:"foreignKey:PembayaranID" json:"transaksi"`
-	UserID       uuid.UUID  `gorm:"type:uuid" json:"user_id"`
-	User         User       `gorm:"foreignKey:UserID" json:"user"`
+	Transaksi          []Transaksi `gorm:"foreignKey:PembayaranID" json:"transaksi"`
+	StatusPembayaranID uint        `gorm:"type:uint" json:"status_pembayaran_id"`
+	ListBankID         uint        `gorm:"type:uint" json:"list_bank_id"`
 }
